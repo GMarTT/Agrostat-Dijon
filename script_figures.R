e8fe4fb85dbd64a6381f05a23800212c8e97bf4c
MARTET Gaspard <g.martet@afssa.fr>
2026-08-26 16:41:50 +0200

    update diapo

--- script_figures.R
+++ script_figures.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(patchwork)
+library(pheatmap)
 
 df <- data.frame(nb_cluster = c("8 → 7", "7 → 6", "6 → 5", "5 → 4",
                        "4 → 3", "3 → 2", "2 → 1"),
@@ -21,5 +23,212 @@ g <- df %>%
         axis.title = element_text(size = 15))
 
 
-ggsave("C:/Users/g.martet/Documents/Agrostat Dijon/figures/var_crit_T.png", g, dpi = 400,
-       width = 7, height = 5)
+# ggsave("C:/Users/g.martet/Documents/Agrostat Dijon/figures/var_crit_T.png", g, dpi = 400,
+#        width = 7, height = 5)
+
+#-------------------------------------------------------------------------------
+# figure variable fonctionnelle et variable smoothée
+#-------------------------------------------------------------------------------
+
+dat <- openxlsx::read.xlsx("C:/Users/g.martet/Documents/updatedata/dat.xlsx")
+dim(dat)
+
+dfPC1.AMR_ville_FQ <- openxlsx::read.xlsx("C:/Users/g.martet/Documents/functional_study/df_fPC1/sum.var_AMR_ville_FQ_R.xlsx")
+rownames(dfPC1.AMR_ville_FQ) <- dfPC1.AMR_ville_FQ[,1]
+dfPC1.AMR_ville_FQ <- dfPC1.AMR_ville_FQ[,-1]
+
+data <- dat[, which(colnames(dat) %in% c("region", "annee", v))]             
+ncomp <- 2 
+Nb.Base <- 5
+
+func_data <- function(data, Nb.Base, ncomp){
+  
+  data <- data %>% 
+    dplyr::mutate(annee = annee %>% factor, region = region %>%  factor)
+  
+  # Paramètres
+  Temps      <- levels(data$annee)
+  NbTemps    <- length(Temps)
+  VarName    <- colnames(data)[-c(1:2)]
+  NbVar      <- length(VarName)
+  Regions    <- levels(data$region)
+  NbRegions  <- length(Regions)
+  my_palette <- colorRampPalette(c("blue", "grey", "red"))
+  
+  # Transformation des variables en matrice
+  mat_tmps_reg <- lapply(1:NbVar, function(p) as.matrix(reshape2:::dcast(data, annee  ~ region, value.var = VarName[p])[, -1]))
+  
+  # transformation en objet fonctionnel
+  # Base B-spline
+  spline.basis <- fda::create.bspline.basis(rangeval = c(0, 1), nbasis = Nb.Base, norder = 3)
+  # plot(spline.basis, lty = 1, lwd = 2, main = "Base de B-splines", xlab = 'Temps')
+  # Projection des données dans la base
+  data.amc.fd <- lapply(1:NbVar, function(p) {
+    mat <- mat_tmps_reg[[p]]
+    mat <- cbind(mat, annee = 2012:2024)
+    mat <- na.omit(mat)
+    Temps <- levels(as.factor(mat[, "annee"]))
+    NbTemps    <- length(Temps)
+    mat <- mat[,-which(colnames(mat) == "annee")]
+    Time.01      <- seq(0, 1, length.out = NbTemps)
+    fda::smooth.basis(Time.01, mat, spline.basis)$fd})
+  
+  # fcpa
+  fPCA.amc <- lapply(1:NbVar, function(p) fda::pca.fd(data.amc.fd[[p]], nharm = ncomp, centerfns = TRUE))
+  names(fPCA.amc) <- VarName
+  df <- data.frame(col1 = rep(NA,12))
+  for (j in 1:NbVar){
+    d <- as.data.frame(fPCA.amc[[j]]$scores)
+    colnames(d) <- paste0(VarName[j], "_fPC", 1:ncol(d))
+    df <- cbind.data.frame(df, d)
+  }
+  df <- df[,-1]
+  rownames(df) <- c("Auvergne-Rhone-Alpes", "Bourgogne-Franche-Comte", "Bretagne",               
+                    "Centre-Val de Loire", "Grand Est", "Hauts-de-France", "IDF", "Normandie",               "Nouvelle-Aquitaine", "Occitanie", "PACA", "Pays de la Loire")
+  return(list(df, data.amc.fd, fPCA.amc))
+}
+
+v <- intersect(colnames(dat), colnames(dfPC1.AMR_ville_FQ))
+dat.func_ville_FQ <- func_data(data = dat[, which(colnames(dat) %in% c("region", "annee", v))],              
+                               ncomp = 2, Nb.Base = 5)
+
+# 1) Plot de la variable fonctionelle
+
+cols <- c("IDF" = "#FFD700", 
+          "Auvergne-Rhone-Alpes" = "#FB6A4A", "PACA" = "#CB181D", 
+          "Nouvelle-Aquitaine" = "#FD8D3C", "Occitanie" = "#D94701",
+          "Centre-Val de Loire" = "#9ECAE1", "Pays de la Loire" = "#6BAED6",
+          "Normandie" = "#3182BD", "Bretagne" = "#08519C", 
+          "Bourgogne-Franche-Comte" = "#74C476", "Grand Est" = "#31A354", 
+          "Hauts-de-France" = "#006D2C")
+
+g1 <- dat %>% 
+  dplyr::filter(annee != 2024) %>% 
+  dplyr::select(region, annee, log_AMC_ville_FQ) %>% 
+  tidyr::pivot_longer(!c("region", "annee"), names_to = "Var",
+                      values_to = "value") %>%
+  ggplot(aes(x = annee, y = value, col = region))+
+  geom_point()+
+  geom_line(show.legend = FALSE)+
+  theme_bw()+
+  labs(color = "Région",
+       title = "Log Fluoroquinolone Consumption",
+       y = "DDJ/1000 habitants")+
+  scale_x_continuous(breaks = seq(2012, 2024, by = 1),
+                     minor_breaks = seq(2012, 2024, by = 1))+
+  theme(axis.text.y = element_text(size = 6.5, color = "black"),
+        axis.title.x = element_blank(),
+        axis.title.y = element_text(size = 6.5),
+        plot.title = element_text(hjust = 0.5, size = 7.5),
+        axis.text.x = element_text(angle = 45, vjust = 0.6, colour = "black", size = 6.5), 
+        legend.text = element_text(size = 6.5), 
+        legend.title = element_text(size = 6.5),
+        legend.position = "bottom",
+        legend.justification = "left",
+        legend.spacing = unit(0.5, "cm"),        
+        legend.key.height = unit(0.1, "cm"),   
+        legend.key.width = unit(0.1, "cm"),    
+        legend.key.spacing.y = unit(0.1, "cm") 
+  )+
+  scale_color_manual(values = cols)
+
+ggsave("C:/Users/g.martet/Documents/Agrostat Dijon/figures/var_AMC_FQ_ville.png", g1, dpi = 400,
+       width = 5, height = 4)
+
+# 2) plot de la variable smoothée
+
+data <- dat %>% 
+  dplyr::mutate(annee = annee %>% factor, region = region %>%  factor)
+
+# Paramètres
+Temps      <- levels(data$annee)
+NbTemps    <- length(Temps)
+#VarName    <- colnames(data)[-c(1:2)]
+#NbVar      <- length(VarName)
+Regions    <- levels(data$region)
+NbRegions  <- length(Regions)
+my_palette <- colorRampPalette(c("blue", "grey", "red"))
+
+# Transformation des variables en matrice
+M <- data %>% 
+  dplyr::select(region, annee, log_AMC_ville_FQ) %>% 
+  tidyr::pivot_wider(names_from = region, values_from = log_AMC_ville_FQ)
+
+mat_tmps_reg <- as.matrix(M[, -1])
+
+# transformation en objet fonctionnel
+# Base B-spline
+spline.basis <- fda::create.bspline.basis(rangeval = c(0, 1), nbasis = Nb.Base, norder = 3)
+
+mat <- mat_tmps_reg
+mat <- cbind(mat, annee = 2012:2024)
+mat <- na.omit(mat)
+Temps <- levels(as.factor(mat[, "annee"]))
+NbTemps    <- length(Temps)
+mat <- mat[,-which(colnames(mat) == "annee")]
+Time.01      <- seq(0, 1, length.out = NbTemps)
+Time.02      <- seq(0, 1, length.out = 1000)
+out.smooth <- fda::smooth.basis(Time.01, mat, spline.basis)
+plot(out.smooth$fd)
+ysmooth <- fda::eval.fd(Time.01, out.smooth$fd)
+ysmooth <- as.data.frame(ysmooth)
+ysmooth$annee <-2012:2023
+
+g2 <- ysmooth %>% 
+  tidyr::pivot_longer(!c("annee"), names_to = "region", values_to = "value") %>% 
+  ggplot(aes(x = annee, y = value, col = region))+
+  geom_point()+
+  geom_line(show.legend = FALSE)+
+  theme_bw()+
+  labs(color = "Région",
+       title = "Smoothed Log Fluoroquinolone Consumption in B-spline basis",
+       y = "DDJ/1000 habitants")+
+  scale_x_continuous(breaks = seq(2012, 2024, by = 1),
+                     minor_breaks = seq(2012, 2024, by = 1))+
+  theme(axis.text.y = element_text(size = 6.5, color = "black"),
+        axis.title.x = element_blank(),
+        axis.title.y = element_text(size = 6.5),
+        plot.title = element_text(hjust = 0.5, size = 7.5),
+        axis.text.x = element_text(angle = 45, vjust = 0.6, colour = "black", size = 6.5), 
+        legend.text = element_text(size = 6.5), 
+        legend.title = element_text(size = 6.5),
+        legend.position = "bottom",
+        legend.justification = "left",
+        legend.spacing = unit(0.5, "cm"),        
+        legend.key.height = unit(0.1, "cm"),   
+        legend.key.width = unit(0.1, "cm"),    
+        legend.key.spacing.y = unit(0.1, "cm") 
+  )+
+  scale_color_manual(values = cols)
+
+ggsave("C:/Users/g.martet/Documents/Agrostat Dijon/figures/smoothed_AMC_FQ_ville.png", g2, dpi = 400,
+       width = 5, height = 4)
+
+ggpubr::ggarrange(g1, g2, common.legend = TRUE)
+
+# Heatmap fPCA
+
+df.heatmap.fPCA <- dfPC1.AMR_ville_FQ %>% 
+  dplyr::select(log_AMC_ville_FQ) %>% 
+  dplyr::arrange(log_AMC_ville_FQ)
+
+?pheatmap::pheatmap
+df.plot <- t(df.heatmap.fPCA)
+rownames(df.plot) <- "fPC1 (98.9%) \nLog Fluoroquinolone \nConsumption"
+  
+ph <- pheatmap::pheatmap(df.plot, 
+                    cluster_rows = FALSE, 
+                    cluster_cols = FALSE,
+                    color = colorRampPalette(c("blue", "white", "red"))(100),
+                    cellwidth = 20,
+                    cellheight = 20,
+                    fontsize = 10,
+                    border_color = "white",
+                    angle_col = 45, 
+                    display_numbers = TRUE, 
+                    number_color = "white")
+
+
+ggsave("C:/Users/g.martet/Documents/Agrostat Dijon/figures/heatmap_fPC1.png", ph, dpi = 400,
+       width = 10, height = 3, bg = "white")
+
